d3d6f3db6ec1954ab17ac51a4b60f900568cc34b
Александр Сотников <sotnikovalex143@gmail.com>
2021-03-01 11:18:24 +0600

    V 0.0.0.0.3

--- app/src/main/java/avastudio/example/newstime/MainActivity.java
+++ app/src/main/java/avastudio/example/newstime/MainActivity.java
@@ -3,12 +3,16 @@ package avastudio.example.newstime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static TextView tvCourse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvCourse = findViewById(R.id.tvCourse);
     }
 }
